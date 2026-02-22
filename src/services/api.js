3dee688f90b28b287ee12bc18d2c334b47a7c879
Tejas R M <112715629+Tejas2913@users.noreply.github.com>
2026-02-22 17:49:43 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://fraudshield-backend-4.onrender.com',
+  baseURL: 'https://fraudshield-backend-5.onrender.com',
 })
 
 // Attach JWT token to every request automatically
